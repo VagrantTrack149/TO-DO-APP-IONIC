fa3a5e1b54e62ef66582b96106d2a2022f3eefc5
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2025-07-17 16:43:51 -0600

    Añadiendo que comience en home para ver la lista de tareas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { TodoService } from '../services/todo.service';
 import { ModalController } from '@ionic/angular';
-import { AddTodoComponent } from '../modals/add-todo/add-todo.page';
+import { AddTodoComponent as AddTodoPage } from '../modals/add-todo/add-todo.page';
 
 
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: './home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule,FormsModule,IonicModule],
 })
 export class HomePage {
   todos:any[]=[];
--- src/app/modals/add-todo/add-todo.page.ts
+++ src/app/modals/add-todo/add-todo.page.ts
@@ -1,32 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { TodoService } from 'src/app/services/todo.service';
+import { Component } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { TodoService } from '../../services/todo.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.page.html',
   styleUrls: ['./add-todo.page.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule]
 })
-export class AddTodoComponent  implements OnInit {
+export class AddTodoComponent {
   title: string = '';
   description: string = '';
   tags: string[] = [];
-  dueDate: string="";
+  dueDate: string = "";
 
   constructor(
     private modalController: ModalController,
     private todoService: TodoService
   ) { }
-  dismiss(){
+
+  dismiss() {
     this.modalController.dismiss();
   }
-  addTodo(){
+
+  addTodo() {
     if (this.title.trim() !== '' && this.description.trim() !== '') {
-      const dueDate = this.dueDate ? new Date(this.dueDate) : undefined;
-      this.todoService.addTodo(this.title, this.description, this.tags, dueDate);
+      const dueDateObj = this.dueDate ? new Date(this.dueDate) : undefined;
+      this.todoService.addTodo(this.title, this.description, this.tags, dueDateObj);
       this.modalController.dismiss();
     }
   }
-  ngOnInit() {}
-
 }
